eb0a7b1566ace1ad2a12c2655c11072e4dddcf4e
Vitor Meira Gomes <vitorg.dev@gmail.com>
2020-02-02 03:59:50 -0300

    Yarn and snyk

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const UserController = require("../controllers/UserController");
 module.exports = {
   async register(app) {
     app.use("/v1/auth", UserController.login);
-    app.use(auth);
+    // app.use(auth);
     app.use("/v1", require("./api"));
     app.get("/", ({ res }) => {
       res.json({
--- test/user.spec.js
+++ test/user.spec.js
@@ -1,108 +1,119 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
 let mongoose = require("mongoose");
-let User = require('../src/models/User');
+let User = require("../src/models/User");
 
 //Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
 let should = chai.should();
 
-const dataProvider = ["createdAt", "stats", "_id", "username", "name", "password", "departaments"];
+const dataProvider = [
+  "createdAt",
+  "status",
+  "_id",
+  "username",
+  "name",
+  "password",
+  "departaments"
+];
 
 chai.use(chaiHttp);
 //Our parent block
 /*
-  * Test the /GET route
-  */
+ * Test the /GET route
+ */
 
-describe('/POST user', () => {
-    let userCreated = "";
-    it('it should POST a user', (done) => {
-        let user = {
-            username: "Vitor Arualiado",
-            name: "Vitor",
-            password: "123"
-        }
-        chai.request(server)
-            .post('/v1/users')
-            .send(user)
+describe("/POST user", () => {
+  let userCreated = "";
+  it("it should POST a user", done => {
+    let user = {
+      username: "Vitor Arualiado",
+      name: "Vitor",
+      password: "123"
+    };
+    chai
+      .request(server)
+      .post("/v1/users")
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        dataProvider.forEach(element => {
+          userCreated = res.body.data;
+          res.body.data.should.have.own.property(element);
+        });
+        done();
+      });
+  });
+
+  describe("/GET user", function() {
+    it(`it should GET a one user`, function(done) {
+      chai
+        .request(server)
+        .get(`/v1/users/${userCreated._id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          dataProvider.forEach(element => {
+            res.body.data.should.have.own.property(element);
+          });
+          done();
+        });
+    });
+    describe("/PUT user", function() {
+      it(`it should UPDATE a one user`, function(done) {
+        let userUpdated = {
+          username: "sadasdasdasdasd",
+          name: "Vitor2",
+          password: "1232"
+        };
+        chai
+          .request(server)
+          .put(`/v1/users/${userCreated._id}`)
+          .send(userUpdated)
+          .end((err, res) => {
+            res.should.have.status(200);
+            dataProvider.forEach(element => {
+              res.body.data.should.have.own.property(element);
+            });
+            for (const key in userUpdated) {
+              if (userUpdated.hasOwnProperty(key)) {
+                res.body.data[key].should.to.equal(userUpdated[key]);
+              }
+            }
+            done();
+          });
+      });
+
+      describe("/GET users", () => {
+        it("it should GET all the Users", done => {
+          chai
+            .request(server)
+            .get("/v1/users")
             .end((err, res) => {
+              res.should.have.status(200);
+              dataProvider.forEach(element => {
+                res.body.data[0].should.have.own.property(element);
+              });
+              done();
+            });
+        });
+
+        describe("/DELETE user", function() {
+          it(`it should DELETE a one user`, function(done) {
+            chai
+              .request(server)
+              .del(`/v1/users/${userCreated._id}`)
+              .end((err, res) => {
                 res.should.have.status(200);
                 dataProvider.forEach(element => {
-                    userCreated = res.body.data;
-                    res.body.data.should.have.own.property(element);
+                  res.body.data.should.have.own.property(element);
                 });
                 done();
-            });
-    });
-
-
-    describe('/GET user', function () {
-        it(`it should GET a one user`, function (done) {
-            chai.request(server)
-                .get(`/v1/users/${userCreated._id}`)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    dataProvider.forEach(element => {
-                        res.body.data.should.have.own.property(element);
-                    });
-                    done();
-                });
+              });
+          });
         });
-        describe('/PUT user', function () {
-            it(`it should UPDATE a one user`, function (done) {
-                let userUpdated = {
-                    username: "sadasdasdasdasd",
-                    name: "Vitor2",
-                    password: "1232"
-                }
-                chai.request(server)
-                    .put(`/v1/users/${userCreated._id}`)
-                    .send(userUpdated)
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        dataProvider.forEach(element => {
-                            res.body.data.should.have.own.property(element);
-                        });
-                        for (const key in userUpdated) {
-                            if (userUpdated.hasOwnProperty(key)) {
-                                res.body.data[key].should.to.equal(userUpdated[key])
-                            }
-                        }
-                        done();
-                    });
-            });
-
-            describe('/GET users', () => {
-                it('it should GET all the Users', (done) => {
-                    chai.request(server)
-                        .get('/v1/users')
-                        .end((err, res) => {
-                            res.should.have.status(200);
-                            dataProvider.forEach(element => {
-                                res.body.data[0].should.have.own.property(element);
-                            });
-                            done();
-                        });
-                });
-
-
-                describe('/DELETE user', function () {
-                    it(`it should DELETE a one user`, function (done) {
-                        chai.request(server)
-                            .del(`/v1/users/${userCreated._id}`)
-                            .end((err, res) => {
-                                res.should.have.status(200);
-                                dataProvider.forEach(element => {
-                                    res.body.data.should.have.own.property(element);
-                                });
-                                done();
-                            });
-                    });
-                });
-            });
-        });
+      });
     });
+  });
 });
